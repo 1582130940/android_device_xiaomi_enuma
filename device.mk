3c09f7e747c73ccdd55cb85cf1a3328c02bb5561
Tim Zimmermann <tim@linux4.de>
2023-12-25 01:57:50 +0800

    enuma: device: Enable support for freeform windows and picture-in-picture
    Change-Id: If0a63fc7e8455100128fa25955bb4da485fff873

--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml \
+    frameworks/native/data/etc/android.software.picture_in_picture.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.picture_in_picture.xml
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
